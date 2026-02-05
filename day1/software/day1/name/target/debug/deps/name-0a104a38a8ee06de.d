4632b8351c993b2b7955d37cd543080ab2367e64
Skeezko <heshamch2005@gmail.com>
2026-02-05 02:02:31 +0100

    Delete day1/software/day1/name/target directory

--- day1/software/day1/name/target/debug/deps/name-0a104a38a8ee06de.d
+++
@@ -1,6 +0,0 @@
-/home/shami/Epitech/Poc-Software/day1/software/day1/name/target/debug/deps/name-0a104a38a8ee06de.d: src/main.rs src/what_is_your_name.rs
-
-/home/shami/Epitech/Poc-Software/day1/software/day1/name/target/debug/deps/name-0a104a38a8ee06de: src/main.rs src/what_is_your_name.rs
-
-src/main.rs:
-src/what_is_your_name.rs:
